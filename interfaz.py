e4015efe538e39839b483d08707ba067eeee17a0
OcampoGod <161398884+OcampoGod@users.noreply.github.com>
2024-02-27 17:50:02 -0500

    Delete interfaz.py

--- interfaz.py
+++
@@ -1,5 +0,0 @@
-def menu():
-    print("\nBIENVENIDO A DATOS ABIERTOS COLOMBIA, EN ESTA SECCIÓN PODRAS CONSULTAR LOS CASOS DE COVID-19 EN EL PAÍS")
-    limite_registros = int(input("Digite el limite de registros: "))
-    nombredepartamento = input("Digite el departamento (EN MAYUSCULA): ")
-    return [limite_registros, nombredepartamento]
